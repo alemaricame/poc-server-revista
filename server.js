a460b6396b1d0c933412b5a0fe7279e82dbfa370
ale <ale@tribuu.com>
2021-03-07 22:28:54 -0600

    fix(mongoose)

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 
 app.get('/heroku', (req, res) => {
 	res.send("Probando")
 })
 
+
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.xp3qr.mongodb.net/poc?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+})
+
+mongoose.connection.on('connected', () => {
+    console.log('mongose is connected')
+})
 app.listen( process.env.PORT ||3000, () => {
 	console.log("Server Listen in port ")
 })
