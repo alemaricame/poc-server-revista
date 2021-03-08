27e1eea8341abcc5bba89d61cb0d4981e0ab817e
ale <ale@tribuu.com>
2021-03-07 23:01:33 -0600

    fix(cors install)

--- server.js
+++ server.js
@@ -11,16 +11,24 @@ const path = require('path');
 
 require("./routes/route")(app);
 
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-	next();
-});
+app.use(function (req, res, next) {
 
-var cors = require('cors')
-app.use(cors());
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://poc-server-heroku.herokuapp.com/api/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 mongoose.connect('mongodb+srv://admin:admin@cluster0.xp3qr.mongodb.net/poc?retryWrites=true&w=majority', {
     useNewUrlParser: true,
