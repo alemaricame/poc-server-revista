e426010c8c810296110ff23e3166c9508480425d
ale <ale@tribuu.com>
2021-03-07 22:54:41 -0600

    fix(cors)

--- server.js
+++ server.js
@@ -10,7 +10,16 @@ const path = require('path');
 // })
 
 require("./routes/route")(app);
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
 
+var cors = require('cors')
+app.use(cors())
 
 mongoose.connect('mongodb+srv://admin:admin@cluster0.xp3qr.mongodb.net/poc?retryWrites=true&w=majority', {
     useNewUrlParser: true,
