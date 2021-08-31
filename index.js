ec9cb43a16cb6ca6d36098a54f3aed9ca4957df5
Alejandra María del Carmen Sánchez Acosta <alejandra.sanchez@mediatasoft.com>
2021-08-31 02:15:44 -0500

    cors

--- index.js
+++ index.js
@@ -39,8 +39,27 @@ app.get('/heroku', (req, res) => {
 })
 
 var cors = require('cors')
-app.use(cors())
 
+const allowedOrigins = [
+  'capacitor://localhost',
+  'ionic://localhost',
+  'http://localhost',
+  'http://localhost:8080',
+  'http://localhost:8100'
+];
+// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  }
+}
+
+// Enable preflight requests for all routes
+app.options('*', cors(corsOptions));
 app.set('port', 5050);
 
 app.listen(5050, function () {
